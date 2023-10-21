70a83e64fc28fefa99da3393c32ee304f0188ae7
Danishaft <danielejeh2019@gmail.com>
2023-10-21 10:38:37 +0100

    worked on the form and inputs

--- comp/CreateItem/CreateItem.tsx
+++ comp/CreateItem/CreateItem.tsx
@@ -1,16 +1,45 @@
+'use client'
+import { useState, ChangeEvent } from 'react'
 import { Button } from '..'
 import styles from './CreateItem.module.scss'
+
+interface Input {
+  name: string;
+  note: string;
+  image: string;
+  category: string;
+}
 export const CreateItem = () => {
+  const initialInput: Input = {
+    name: '',
+    note: '',
+    image: '',
+    category: '',
+  };
+  const [inputVal, setInputVal] = useState<Input>(initialInput)
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const {name, value} = e.target
+    setInputVal({
+      ...inputVal,
+      [name]: value
+    })
+  }
   return (
     <section className={styles.createItem_sec}>
       <div className={styles.cont}>
         <h2>Add a new item</h2>
         <form>
-          <label htmlFor="name">Name</label><br />
-          <input type="text" name='name' placeholder='Enter a name' /><br />
+          <label htmlFor="name">Name</label>
+          <input value={inputVal?.name} onChange={handleChange} className={styles.input} type="text" name='name' placeholder='Enter a name' />
 
-          <label htmlFor="note">Note</label><br/>
-          <textarea name='note' placeholder='Enter a note' />
+          <label htmlFor="note">Note(optional)</label>
+          <textarea value={inputVal?.note} onChange={handleChange} className={styles.input} name='note' placeholder='Enter a note' />
+
+          <label htmlFor='image'>Image(optional)</label>
+          <input value={inputVal?.image} onChange={handleChange} className={styles.input} type='text' name='image' placeholder='Enter a url'/>
+
+          <label htmlFor='category'>Category</label>
+          <input value={inputVal?.category} onChange={handleChange} className={styles.input} type='text' name='category' placeholder='Enter a category'/>
         </form>
       </div>
       <div className={styles.bottom}>
